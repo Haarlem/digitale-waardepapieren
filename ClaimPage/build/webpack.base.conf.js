885a1202bc1551304375ab546f98d332800e16e1
Peter <peter@codebuffet.co>
2017-12-04 22:48:07 +0100

    started on copying haarlem page

--- ClaimPage/build/webpack.base.conf.js
+++ ClaimPage/build/webpack.base.conf.js
@@ -29,6 +29,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
