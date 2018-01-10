ff11be44936bcc164cd89505b71a496824f4b66e
Peter <peter@codebuffet.co>
2018-01-10 16:50:43 +0100

    removed browsersync (is broken)

--- Scanner/Scanner-Mobile/platforms/browser/platform_www/cordova_plugins.js
+++ Scanner/Scanner-Mobile/platforms/browser/platform_www/cordova_plugins.js
@@ -19,8 +19,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-qrscanner": "2.5.0",
-    "cordova-plugin-browsersync": "0.1.7"
+    "cordova-plugin-qrscanner": "2.5.0"
 }
 // BOTTOM OF METADATA
 });
